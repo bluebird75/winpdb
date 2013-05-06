b8eb79c1458427671d4a0ac88a7cb5060fa9a8b0
Bluebird75 <phil@freehackers.org>
2013-05-06 23:20:34 +0200

    Cleanup
    - make names more meaningful
    - do not detached if already detached
    - use prefix for output to make script, debugger and debugger output more readable

--- test_rpdb2.py
+++ test_rpdb2.py
@@ -9,23 +9,29 @@ DEBUGME=u'debugme.py'
 RPDB2 = 'rpdb2.py'
 PWD=u'toto'
 
-class MyStdin:
+class FakeStdin:
     def __init__(self):
         self.please_stop = False
         self.lines = []
+        self.dispStdin = True
+
+    def appendCmd( self, l ):
+        cmd = l if (l[-1] == '\n') else (l + '\n')
+        self.lines.append( l )
 
     def readline(self):
         while not self.please_stop:
            if len(self.lines):
-            p = self.lines.pop(0)
-            print 'stdin:', p
-            return p
+                p = self.lines.pop(0)
+                if self.dispStdin:
+                    sys.stdout.write( 'stdin: %s\n' % (p[:-1] if p[-1] == '\n' else p) )
+                return p
         time.sleep(0.1)
 
 def dbg( t ):
     print '>>>>>>', t, '<<<<<<'
 
-class CopyStdout(io.StringIO):
+class Rpdb2Stdout(io.StringIO):
     def __init__(self, *args):
         io.StringIO.__init__(self, *args)
         self.attached = False
@@ -42,9 +48,9 @@ class CopyStdout(io.StringIO):
         sys.stdout.write( '%s' % t )
 
 
-class TestCopyStdout( unittest.TestCase ):
+class TestRpdb2Stdout( unittest.TestCase ):
     def testReAttached( self ):
-        self.assertNotEquals( CopyStdout.reAttached.match( '*** Successfully attached to\n' ), None )
+        self.assertNotEquals( Rpdb2Stdout.reAttached.match( '*** Successfully attached to\n' ), None )
 
 
 class TestRpdb2( unittest.TestCase ):
@@ -52,57 +58,55 @@ class TestRpdb2( unittest.TestCase ):
     def setUp(self):
         self.console = None
         self.sm = None
-        self.myStdin = MyStdin()
+        self.fakeStdin = FakeStdin()
+        self.rpdb2Stdout = Rpdb2Stdout()
         self.script = subprocess.Popen( [ PYTHON, RPDB2, '-d', '--pwd=%s' % PWD, os.path.join( 'tests', DEBUGME ) ], 
-                        creationflags=subprocess.CREATE_NEW_PROCESS_GROUP ) # stdout=subprocess.PIPE, stderr=subprocess.STDOUT )
-        # script.wait()
-        # scriptStdout = script.stdout.read()
-        # print scriptStdout
+                        creationflags=subprocess.CREATE_NEW_PROCESS_GROUP, stdout=subprocess.PIPE, stderr=subprocess.STDOUT )
 
     def tearDown(self):
 
         dbg( 'Teardown' )
         if self.console:
-            dbg( 'Running script until the end' )
-            self.myStdin.lines.append('go\n')
+
+            if self.rpdb2Stdout.attached:
+                dbg( 'Teardown: Stopping...' )
+                self.fakeStdin.appendCmd('stop\n')
+                time.sleep(1.0)
+
+                #dbg( 'Teardown: Detaching...' )
+                #self.fakeStdin.appendCmd('detach\n')
+                #time.sleep(1.0)
+
+            dbg( 'Teardown: Exiting...' )
+            self.fakeStdin.appendCmd('exit\n')
             time.sleep(1.0)
-            dbg( 'Detaching...' )
-            self.myStdin.lines.append('detach\n')
-            time.sleep(1.0)
-            dbg( 'Exiting...' )
-            self.myStdin.lines.append('exit\n')
-            time.sleep(1.0)
-            dbg( 'console exit ok' )
+
+            # dbg( 'console exit ok' )
             self.console.join( 1.0 )
-            dbg( 'console join ok' )
+            # dbg( 'console join ok' )
             self.sm.shutdown()
-            dbg( 'sm shutdown ok' )
+            # dbg( 'sm shutdown ok' )
             time.sleep(1.0)
-            dbg( 'console done' )
+            dbg( 'Teardown: Console done' )
 
 
-        print 'tearDown: attempt1'
+        dbg( 'Teardown.Script: check if finished')
         if self.script.poll() != None: return
 
+        dbg( 'Teardown.Script: check if finished after 1 sec')
         time.sleep(1.0)
-        print 'tearDown: attempt2'
         if self.script.poll() != None: return
 
-        print 'tearDown: attempt3'
+        dbg( 'Teardown.Script: terminate()')
         self.script.terminate()
         if self.script.poll() != None: return
-
-        # script not terminated yet
         time.sleep(1.0)
-        print 'tearDown: attempt4'
         if self.script.poll() != None: return
 
+        dbg( 'Teardown.Script: kill()')
         self.script.kill()
-        print 'tearDown: attempt5'
         if self.script.poll() != None: return
-
         time.sleep(1.0)
-        print 'tearDown: attempt6'
         if self.script.poll() != None: return
 
         raise ValueError( 'Error, script not terminated: pid=%d' % self.script.pid )
@@ -118,22 +122,23 @@ class TestRpdb2( unittest.TestCase ):
         command_line=DEBUGME
         fSplit=True
 
-        copyStdout = CopyStdout()
-
         self.sm = rpdb2.CSessionManager(_rpdb2_pwd, fAllowUnencrypted, fAllowRemote, host)
-        self.console = rpdb2.CConsoleInternal(self.sm, stdout=copyStdout, stdin=self.myStdin, fSplit=fSplit )
+        self.console = rpdb2.CConsoleInternal(self.sm, stdout=self.rpdb2Stdout, stdin=self.fakeStdin, fSplit=fSplit )
         self.console.start()
 
         time.sleep(1.0)
 
-        self.myStdin.lines.append( "attach %s\n" % DEBUGME )
+        self.fakeStdin.appendCmd( "attach %s\n" % DEBUGME )
         startCountDown = time.time()
         while time.time() - startCountDown < 10.0:
             time.sleep(1.0)
-            if copyStdout.attached: 
+            if self.rpdb2Stdout.attached: 
                 break
-        self.assertEquals( copyStdout.attached, True )
+        self.assertEquals( self.rpdb2Stdout.attached, True )
 
+        self.fakeStdin.appendCmd( "go" )
+        time.sleep(1.0)
+        # print self.script.stdout.read()
 
 if __name__ == '__main__':
     unittest.main()
