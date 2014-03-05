4e47d589de58b5b04d8bd11a9c4dcf7f61586caf
Bluebird75 <75.bluebird@gmail.com>
2014-03-05 13:29:38 +0100

    Make debugme.py compatible with Python 3 and 2

--- tests/debugme.py
+++ tests/debugme.py
@@ -1,8 +1,17 @@
+import sys
+
+def is_py3k():
+    return sys.version_info[0] >= 3
+
+if is_py3k():
+    def u(s): return s
+else:
+    def u(s): return unicode(s)
 
 prefix = 'DEBUGME: '
 
 def step(t):
-    print( prefix + unicode(t) )
+    print( prefix + u(t) )
     open( t, 'w' ).write(t+'\n')
 
 def f1( t ):
