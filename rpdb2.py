8530fdfb3d10215ee3857f64f91f9d41b0e82052
Philippe F <phil.fremy@free.fr>
2020-06-03 07:08:23 +0200

    Move exceptions to dedicated module

--- rpdb2.py
+++ rpdb2.py
@@ -32,7 +32,11 @@ from src.events import CEventNull, CEventEmbeddedSync, CEventClearSourceCache, C
     CEventForkSwitch, CEventExecSwitch, CEventExit, CEventState, CEventSynchronicity, CEventBreakOnExit, CEventTrap, \
     CEventForkMode, CEventUnhandledException, CEventNamespace, CEventNoThreads, CEventThreads, CEventThreadBroken, \
     CEventStack, CEventStackFrameChange, CEventStackDepth, CEventBreakpoint, CEventSync, breakpoint_copy
-from src.exceptions import InvalidScopeName, CException
+from src.exceptions import InvalidScopeName, CException, BadMBCSPath, NotPythonSource, BadArgument, ThreadNotFound, \
+    NoThreads, ThreadDone, DebuggerNotBroken, InvalidFrame, NoExceptionFound, CConnectionException, FirewallBlock, \
+    BadVersion, UnexpectedData, AlreadyAttached, NotAttached, SpawnUnsupported, UnknownServer, UnsetPassword, \
+    EncryptionNotSupported, EncryptionExpected, DecryptionFailure, AuthenticationBadData, AuthenticationFailure, \
+    AuthenticationBadIndex, g_error_mapping
 from src.globals import g_fDebug, g_traceback_lock
 from src.utils import is_unicode, as_unicode, as_string, as_bytes, print_debug, print_debug_exception, winlower, _print, \
     print_exception, thread_set_daemon, thread_is_alive, thread_set_name, thread_get_name, current_thread, \
@@ -1235,172 +1239,6 @@ class CConsole:
 #
 
 
-class BadMBCSPath(CException):
-    """
-    Raised on Windows systems when the python executable or debugger script
-    path can not be encoded with the file system code page. This means that
-    the Windows code page is misconfigured.
-    """
-
-
-
-class NotPythonSource(CException):
-    """
-    Raised when an attempt to load non Python source is made.
-    """
-
-
-class BadArgument(CException):
-    """
-    Bad Argument.
-    """
-
-
-
-class ThreadNotFound(CException):
-    """
-    Thread not found.
-    """
-
-
-
-class NoThreads(CException):
-    """
-    No Threads.
-    """
-
-
-
-class ThreadDone(CException):
-    """
-    Thread Done.
-    """
-
-
-
-class DebuggerNotBroken(CException):
-    """
-    Debugger is not broken.
-    This exception is thrown when an operation that can only be performed
-    while the debuggee is broken, is requested while the debuggee is running.
-    """
-
-
-
-class InvalidFrame(CException):
-    """
-    Invalid Frame.
-    This exception is raised if an operation is requested on a stack frame
-    that does not exist.
-    """
-
-
-
-class NoExceptionFound(CException):
-    """
-    No Exception Found.
-    This exception is raised when exception information is requested, but no
-    exception is found, or has been thrown.
-    """
-
-
-
-class CConnectionException(CException):
-    def __init__(self, *args):
-        CException.__init__(self, *args)
-
-
-
-class FirewallBlock(CConnectionException):
-    """Firewall is blocking socket communication."""
-
-
-
-class BadVersion(CConnectionException):
-    """Bad Version."""
-    def __init__(self, version):
-        CConnectionException.__init__(self)
-
-        self.m_version = version
-
-    def __str__(self):
-        return repr(self.m_version)
-
-
-
-class UnexpectedData(CConnectionException):
-    """Unexpected data."""
-
-
-
-class AlreadyAttached(CConnectionException):
-    """Already Attached."""
-
-
-
-class NotAttached(CConnectionException):
-    """Not Attached."""
-
-
-
-class SpawnUnsupported(CConnectionException):
-    """Spawn Unsupported."""
-
-
-
-class UnknownServer(CConnectionException):
-    """Unknown Server."""
-
-
-
-class CSecurityException(CConnectionException):
-    def __init__(self, *args):
-        CConnectionException.__init__(self, *args)
-
-
-
-class UnsetPassword(CSecurityException):
-    """Unset Password."""
-
-
-
-class EncryptionNotSupported(CSecurityException):
-    """Encryption Not Supported."""
-
-
-
-class EncryptionExpected(CSecurityException):
-    """Encryption Expected."""
-
-
-class DecryptionFailure(CSecurityException):
-    """Decryption Failure."""
-
-
-
-class AuthenticationBadData(CSecurityException):
-    """Authentication Bad Data."""
-
-
-
-class AuthenticationFailure(CSecurityException):
-    """Authentication Failure."""
-
-
-
-class AuthenticationBadIndex(CSecurityException):
-    """Authentication Bad Index."""
-    def __init__(self, max_index = 0, anchor = 0):
-        CSecurityException.__init__(self)
-
-        self.m_max_index = max_index
-        self.m_anchor = anchor
-
-    def __str__(self):
-        return repr((self.m_max_index, self.m_anchor))
-
-
-
 #
 #----------------- unicode handling for compatibility with py3k ----------------
 #
@@ -1579,30 +1417,6 @@ g_fScreen = False
 g_fDefaultStd = True
 
 g_initial_cwd = []
-
-g_error_mapping = {
-    socket.error: STR_COMMUNICATION_FAILURE,
-
-    CConnectionException: STR_LOST_CONNECTION,
-    FirewallBlock: STR_FIREWALL_BLOCK,
-    BadVersion: STR_BAD_VERSION,
-    UnexpectedData: STR_UNEXPECTED_DATA,
-    SpawnUnsupported: STR_SPAWN_UNSUPPORTED,
-    UnknownServer: STR_DEBUGGEE_UNKNOWN,
-    UnsetPassword: STR_PASSWORD_MUST_BE_SET,
-    EncryptionNotSupported: STR_DEBUGGEE_NO_ENCRYPTION,
-    EncryptionExpected: STR_ENCRYPTION_EXPECTED,
-    DecryptionFailure: STR_DECRYPTION_FAILURE,
-    AuthenticationBadData: STR_ACCESS_DENIED,
-    AuthenticationFailure: STR_ACCESS_DENIED,
-    BadMBCSPath: STR_BAD_MBCS_PATH,
-
-    AlreadyAttached: STR_ALREADY_ATTACHED,
-    NotAttached: STR_NOT_ATTACHED,
-    DebuggerNotBroken: STR_DEBUGGEE_NOT_BROKEN,
-    NoThreads: STR_NO_THREADS,
-    NoExceptionFound: STR_EXCEPTION_NOT_FOUND,
-}
 
 #
 # These globals are related to handling the os.fork() os._exit() and exec
@@ -6454,7 +6268,7 @@ class CDebuggerEngine(CDebuggerCore):
             - DICT_KEY_CURRENT_TID: boolean, True if this is the current thread
         '''
         if fException and (fAll or (len(tid_list) != 0)):
-            raise BadArgument( "You shall request either Exception, or list of tid, or all")
+            raise BadArgument("You shall request either Exception, or list of tid, or all")
 
         ctx = self.get_current_ctx()
         ctid = ctx.m_thread_id
